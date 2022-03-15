882249d278bd7331edd2dd4a7598d139431b30b7
thaole30 <thaolee3005@gmail.com>
2022-03-15 08:42:27 +0700

    add scroll and loop

--- scripts/script.js
+++ scripts/script.js
@@ -1,49 +1,27 @@
 const carouselList = document.querySelector('.carousel-list');
-const items = document.querySelectorAll('.carousel-list .item');
-// console.log("items", items)
 
 //get btn
 const prevBtn = document.querySelector('#prevBtn');
 const nextBtn = document.querySelector('#nextBtn');
 
-//get size of 1 item
-const itemSize = items[0].offsetWidth;
-console.log(itemSize);
-
-let counter = 1;
-console.log("counter", counter);
-
-
-//dịch chuyển tới slide đầu tiên (pass qua last clone slide)
-carouselList.style.transform = `translateX(-${itemSize}px)`;
-
 nextBtn.addEventListener("click", function () {
-    counter++;
-    console.log("counter", counter);
-    carouselList.style.transform = `translateX(-${counter * itemSize}px)`;
-    carouselList.style.transition = 'transform 1s ease-in-out';
+    console.log("button next")
 
+    const firstItem = carouselList.children[0];
+    firstItem.style.animationName = "fadeOut";
+    firstItem.addEventListener("animationend", function () {
+        firstItem.style.animationName = null;
+        carouselList.appendChild(firstItem);
+    }, {once: true});
 })
-
 
 prevBtn.addEventListener("click", function () {
+    let lastItem = carouselList.children[carouselList.children.length - 1];
+    carouselList.insertBefore(lastItem, carouselList.children[0]);
 
-    counter--;
-    console.log("counter", counter);
-    carouselList.style.transform = `translateX(-${counter * itemSize}px)`;
-    carouselList.style.transition = 'transform 1s ease-in-out';
-})
-
-
-
-carouselList.addEventListener("transitionend", function() {
-    console.log("event transitionend");
-    console.log( "transitionend", items[counter]);
-
-    if(items[counter].id === "firstClone") {
-        counter = items.length - counter;
-        console.log("new counter", counter);
-        carouselList.style.transition = 'none';
-        carouselList.style.transform = `translateX(-${counter * itemSize}px)`
-    }
+    const firstItem = carouselList.children[0];
+    firstItem.style.animationName = "fadeIn";
+    firstItem.addEventListener("animationend", function () {
+        firstItem.style.animationName = null;
+    }, {once: true});
 })
